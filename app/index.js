5a543106ce6ced01c6120fa6065f528cbf37dec0
Jiho Park <jihopark7777@gmail.com>
2022-04-13 04:26:21 +0900

    Feat: Add Persistent Storage

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
 import { Client, Intents } from "discord.js";
 import * as config from './config/config.js';
 import {fMsgHandler} from "./events/message.js";
+import storage from "node-persist";
 
 const { DIRECT_MESSAGES, GUILD_MESSAGES, GUILDS } = Intents.FLAGS;
 const client = new Client({
     intents: [DIRECT_MESSAGES, GUILD_MESSAGES, GUILDS],
     partials: ['CHANNEL', 'MESSAGE'],
 });
+
+// Initialize DB
+await storage.init();
+const OUserDataTest = await storage.getItem('user-data');
+console.log(OUserDataTest);
 
 client.once('ready', () => {
     console.log(`Logged in as ${ client.user.tag }`);
